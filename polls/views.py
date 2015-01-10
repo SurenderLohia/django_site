56bd278118fd05226bd81beab6721cf1d4b255ff
SurenderLohia <surender@fantain.com>
2015-01-10 18:05:53 +0530

    1. Add all files to the git.

---
+++ polls/views.py
@@ -0,0 +1,44 @@
+from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from polls.models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        # Display the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': 'You did not select a choice'
+        })
+
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls.views.results', args=[p.id]))
+
+
